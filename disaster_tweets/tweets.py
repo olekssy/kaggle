21a2bd0cc177df5b543235475c87ed427bcb43ea
Oleksii Lialka <lialka@ualberta.ca>
2020-03-08 02:05:28 +0200

    Added xgboost classifier 0.75

--- disaster_tweets/tweets.py
+++ disaster_tweets/tweets.py
@@ -2,6 +2,12 @@
 that predicts which Tweets are about real disasters and which one’s aren’t.
 https://www.kaggle.com/c/nlp-getting-started/overview """
 
+""" Features:
+1/ get exclamations count
+2/ fill keywords with hashtags
+3/ fill location with countries mentioned in text
+4/ remove top common words for both targets
+"""
 
 import pandas as pd
 import pickle
@@ -9,45 +15,71 @@ import numpy as np
 import re
 import nltk
 
-import keras
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau, Callback
+# import keras
+# from keras.callbacks import EarlyStopping, ReduceLROnPlateau, Callback
 
 import sklearn
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 
-lemmatizer = nltk.stem.WordNetLemmatizer()
+from xgboost import XGBClassifier
+
 
 # --------------------- Load data ---------------------
-data_train = pd.read_csv('train.csv', index_col=0)
-data_test = pd.read_csv('test.csv', index_col=0)
-
 # get countries, cities from https://datahub.io/core/world-cities
-world_cities = pd.read_csv('world-cities.csv', index_col=False)
-countries = set([loc.lower() for loc in world_cities['country']])
+# world_cities = pd.read_csv('world-cities.csv', index_col=False)
+# countries = set([loc.lower() for loc in world_cities['country']])
+
+# NLTK helpers
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+lemmatizer = nltk.stem.WordNetLemmatizer()
+stemmer = nltk.stem.PorterStemmer()
+stopwords = set(nltk.corpus.stopwords.words('english'))
 
 
-# --------------------- Features settings ---------------------
+# --------------------- Helper functions ---------------------
 def clean_text(text):
     text = text.lower()  # text to lowercase
-    text = re.sub(r"[^a-z']+", " ", text)  # keep only letters and digits
-    text = ' '.join(lemmatizer.lemmatize(word) for word in text.split())  # lemmatize tweets
+    text = re.sub(r"[^a-z0-9']+", " ", text)  # keep only letters and digits
+    text = ' '.join(word for word in text.split() if word not in stopwords) # delete stopwors from text
+    text = ' '.join(lemmatizer.lemmatize(word) for word in text.split())  # lemmatize words
+    text = ' '.join(stemmer.stem(word) for word in text.split())  # stem words
     return text
+
+
+def get_exclamations(text):
+    """ Count exclamation marks attached to words or standalone"""
+    ex_count = re.findall("[a-z#]*[!]+", text.lower())
+    return len(ex_count)
+
+
+def get_questions(text):
+    """ Count question marks attached to words or standalone"""
+    quest_count = re.findall("[a-z#]*[\?]+", text.lower())
+    return len(quest_count)
+
+
+def get_caps(text):
+    """ Count CAPS LOCK expressions """
+    caps_count = re.findall("[A-Z][A-Z][A-Z][A-Z]+", text)
+    return len(caps_count)
 
 
 def get_hashtags(text):
     """ Extract keywords as hasshtags from tweets """
-    hashtags = re.findall('#[a-z0-9]+', text)
+    hashtags = re.findall('#[a-z]+', text.lower())
     hashtags = ' '.join([tag.strip('#') for tag in hashtags])
-    return hashtags
+    return clean_text(hashtags)
 
 
 def count_targets(dataY):
     true = np.count_nonzero(dataY)
     false = len(dataY) - true
-    print('True/False: {}/{}'.format(true, false))
+    print('=>Balance T/F: {}/{}'.format(true, false))
     return true, false
 
 
@@ -64,154 +96,113 @@ def balance_data(data):
     return data_balanced
 
 
-def get_features(data):
-    data = balance_data(data)  # balance target values
-    dataX = data['text']
-    dataY = data['target']
+# ---------------------- Feature settings ------------------------
+def prepare_text(data):
+    # data['ex_count'] = data['text'].apply(get_exclamations)  # exclamations count
+    # data['quest_count'] = data['text'].apply(get_questions)  # questions count
+    # data['caps_count'] = data['text'].apply(get_caps)  # CAPS LOCK count
+    # data['tags'] = data['text'].apply(get_hashtags)  # retrieve hashtags
 
-    dataX = data['text'].apply(clean_text)  # clean text
-    # data = data.dropna(how='any')
-    return dataX, dataY
+    data['keyword'] = [word.replace('%20', ' ') if not isinstance(word, float) else np.nan for word in data['keyword']]  # clean keywords
+    data['text'] = data['text'].apply(clean_text)  # clean text
+    # data = data.dropna(axis=0, subset=['keyword'])  # drop missing keywords
+    data['text'] = data['text'] + ' ' + data['keyword'].fillna('')  # merge keywords with text
+
+    # select features
+    # select_labels = ['target', 'ex_count', 'quest_count', 'caps_count', 'text']
+    # data = data[select_labels]  #.values.reshape(-1, data[select_labels].shape[1])
+    # dataY = data['target']
+    # print(dataX)
+    # print(dataX.shape, dataY.shape)
+    return data['text']
 
 
 def set_vectorizer(dataX):
-    # Text vectorizer
-    n_gram = (1, 2) # n-gram sizes for tokenizing text(unigram + bigram + trigram....)
-    token_mode = 'word' # whether text should be split into word or character n-grams
-    min_frequency = 2 # minimum n_gram frequency below which a token will be discarded.
+    # vectorize text
+    vectorizer = TfidfVectorizer(ngram_range = (1, 2),
+    strip_accents = 'unicode',
+    decode_error = 'replace',
+    analyzer = 'word',
+    min_df = 2)
+    vectorized_vocabulary = vectorizer.fit(dataX)
+    pickle.dump(vectorized_vocabulary, open('vectorized_vocabulary.pickle', 'wb'))
 
-    vectorizer = TfidfVectorizer(ngram_range = n_gram,
-                                strip_accents = 'unicode',
-                                decode_error = 'replace',
-                                analyzer = token_mode,
-                                min_df = min_frequency)
-
-    vectorizer_vocabulary = vectorizer.fit(dataX) # Learn vocabulary from training texts
-
-    # Save vocabulary for further predictions usage
-    pickle.dump(vectorizer_vocabulary, open("vectorizer_vocabulary.pickle", "wb"))
-
-    return vectorizer_vocabulary
+    dataX = vectorized_vocabulary.transform(dataX)
+    return dataX
 
 
-# --------------------- Model Settings ---------------------
-def get_estimator(n_features):
+# ------------------------ Model settings ------------------------
+def train_xgboost(trainX, trainY):
+    # vectorize text features
+    trainX = set_vectorizer(trainX)
+    trainX = trainX.reshape(-1, trainX.shape[1])
+    print('=>Vocab vector train', trainX.shape)
 
-    learning_rate = 0.01 # Initial LR (later autotuned with ReduceLROnPlateau)
-
-    inputs = keras.Input(shape=(n_features,))
-    hidden1 = keras.layers.Dense(2048, activation='relu',
-                                       activity_regularizer=keras.regularizers.l1_l2(l1=0.0001, l2=0.0001))(inputs)
-    hidden2 = keras.layers.LeakyReLU(alpha=0.001)(hidden1)
-    hidden3 = keras.layers.Dropout(0.005)(hidden2)
-    hidden4 = keras.layers.Dense(1024, activation='relu',
-                                       activity_regularizer=keras.regularizers.l1_l2(l1=0.0001, l2=0.0001))(hidden3)
-    hidden5 = keras.layers.LeakyReLU(alpha=0.001)(hidden4)
-    hidden6 = keras.layers.Dropout(0.005)(hidden5)
-    hidden7 = keras.layers.Dense(512, activation='relu',
-                                      activity_regularizer=keras.regularizers.l1_l2(l1=0.0001, l2=0.0001))(hidden6)
-    hidden8 = keras.layers.LeakyReLU(alpha=0.001)(hidden7)
-    hidden9 = keras.layers.Dropout(0.005)(hidden8)
-    outputs = keras.layers.Dense(1, activation='sigmoid')(hidden9)
-    model = keras.Model(inputs=inputs, outputs=outputs)
-
-    # Configuration
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate),
-                    loss=keras.losses.binary_crossentropy,
-                    metrics=['accuracy'])
-
-    return model
-
-
-def run_training(trainX, trainY, epochs=1):
-
-    print('Start initial training')
-
-    # Clean text
-    # print('Start text cleaning')
-    # trainX = trainX.apply(clean_text)
-    # Set vectorizer
-    print('Start vectorizing')
-    vectorizer = set_vectorizer(trainX)
-    # Vectorize text
-    trainX = vectorizer.transform(trainX)
-
-    # TUNEME: k parameter
-    # Reduce vocabulary to k best predictors
-    print('Start SelectKbest')
-    selector = SelectKBest(f_classif, k=min(15000, trainX.shape[1]))
+    selector = SelectKBest(f_classif, k=min(5000, trainX.shape[1]))
     selector.fit(trainX, trainY)
-    pickle.dump(selector, open("selector.pickle", "wb"))
-    trainX = selector.transform(trainX).astype('float32')
+    pickle.dump(selector, open('selector.pickle', 'wb'))
+    trainX = selector.transform(trainX)  #.astype('float32')
+    print('=>Reduced vector train', trainX.shape)
 
-    # Declare model
-    model = get_estimator(trainX.shape[1])
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=100, min_delta=0.1)
-    learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss',
-                                                mode='min',
-                                                patience=10,
-                                                verbose=1,
-                                                factor=0.1,
-                                                min_lr=0.00001)
+    # booster: gbtree, gblinear, dart.
+    model = XGBClassifier(max_depth=2, learning_rate=0.01, booster='gblinear', tree_method='exact', reg_alpha=0.0001, reg_lambda=0.0001, verbosity=1)
+    model.fit(trainX, trainY, verbose=True)
+    pickle.dump(model, open('model.pickle', 'wb'))
 
-    # Train model and save
-    model.fit(trainX, trainY, batch_size=20,
-                              validation_split=0.2,
-                              epochs=epochs,
-                              callbacks=[learning_rate_reduction, es],
-                              verbose=1)
-    pickle.dump(model, open("model.pickle", "wb"))
+
+def predict(dataX):
+    # vectorize text features
+    vectorized_vocabulary = pickle.load(open('vectorized_vocabulary.pickle', 'rb'))
+    dataX = vectorized_vocabulary.transform(dataX)
+    print('=>Vocab vector predict', dataX.shape)
+
+    selector = pickle.load(open('selector.pickle', 'rb'))
+    dataX = selector.transform(dataX)  #.astype('float32')
+    print('=>Reduced vector predict', dataX.shape)
+
+    # load model
+    model = pickle.load(open('model.pickle', 'rb'))
+    # predict
+    predY = model.predict(dataX)
+    return predY
 
 
 def run_test(testX, testY):
-    vectorizer = pickle.load(open("vectorizer_vocabulary.pickle", 'rb'))
-    selector = pickle.load(open("selector.pickle", 'rb'))
-    model = pickle.load(open("model.pickle", 'rb'))
-
-    # testX = testX.apply(clean_text)
-    testX = vectorizer.transform(testX)# Vectorize input
-    testX = selector.transform(testX).astype('float32')# Select input
-
-    prediction = model.predict(testX)
-
-    # TUNEME: postprocess threshold manipulation (default 0.5)
-    # prediction = np.argmax(prediction, axis=1)
-    prediction = np.array([1 if i>0.5 else 0 for i in prediction.reshape(-1,)])
-
-    print(classification_report(testY.values.reshape(-1,), prediction))
-
-
-# --------------------- Submission functions ---------------------
-def predict(testX):
-    vectorizer = pickle.load(open("vectorizer_vocabulary.pickle", 'rb'))
-    selector = pickle.load(open("selector.pickle", 'rb'))
-    model = pickle.load(open("model.pickle", 'rb'))
-
-    testX = testX.apply(clean_text)
-    testX = vectorizer.transform(testX)# Vectorize input
-    testX = selector.transform(testX).astype('float32')# Select input
-
-    prediction = model.predict(testX)
-
-    # TUNEME: postprocess threshold manipulation (default 0.5)
-    # prediction = np.argmax(prediction, axis=1)
-    prediction = np.array([1 if i > 0.5 else 0 for i in prediction.reshape(-1,)])
-    return prediction
-
-
-def get_submission(data):
-    testX = data['text']
     predY = predict(testX)
+    # model metrics
+    confMatrix = confusion_matrix(testY, predY)
+    report = classification_report(testY, predY)
+    print(confMatrix)
+    print(report)
+
+
+# --------------------- Submission settings ---------------------
+def get_submission(dataX):
+    predY = predict(dataX)
     # write to csv
-    result = pd.DataFrame({'id': data.index, 'target': predY})
-    result = result.set_index('id')
+    result = pd.DataFrame({'target': predY}, index=dataX.index)
     result.to_csv('submission.csv', index=True)
     print('Submission saved.')
 
 
-dataX, dataY = get_features(data_train)
-trainX, testX, trainY, testY = train_test_split(dataX, dataY, test_size = 0.2, random_state=np.random.randint(0, 100), stratify=dataY)
+def main():
+    data_train = pd.read_csv('train.csv', index_col=0)
+    data_test = pd.read_csv('test.csv', index_col=0)
 
-run_training(trainX, trainY)
-run_test(testX, testY)
-# get_submission(data_test)
+    data_train = balance_data(data_train)  # balance data
+
+    dataX = prepare_text(data_train)
+    dataY = data_train['target']
+
+    trainX, testX, trainY, testY = train_test_split(dataX, dataY, test_size=0.05)
+
+    train_xgboost(trainX, trainY)
+    run_test(testX, testY)
+
+    # predict test
+    # dataX = prepare_text(data_test)
+    # get_submission(dataX)
+
+
+if __name__ == '__main__':
+    main()
